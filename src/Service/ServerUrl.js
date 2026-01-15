fe6655e8237ccce4d21d4d97d2caf9cbcbfdc4ea
Akash-thilakan <akashthilakan2462003@gmil.com>
2026-01-16 00:42:58 +0530

    second commit

--- src/Service/ServerUrl.js
+++ src/Service/ServerUrl.js
@@ -1,2 +1,5 @@
-const SERVERURL = "http://localhost:3000"
+// const SERVERURL = "http://localhost:3000"
+
+const SERVERURL = "https: //eventschedulerbackend-yzs9.onrender.com"
+
 export default SERVERURL
